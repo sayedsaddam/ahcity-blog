4b3114e0fbcb39a33503ea456cd446facdc17cb7
Sayed Saddam Hussain <48250591+sayedsaddam@users.noreply.github.com>
2022-09-27 12:00:55 +0500

    modified 4 files, including the newly directories created at the top level. wrote script for fetching the mardown files created recently for blog posts.

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,18 @@
 import Head from "next/head"
 import Layout, { siteTitle } from "../components/layout"
+import { getSortedPostsData } from "../lib/posts"
 import utilStyles from '../styles/utils.module.css'
 
-export default function Home(){
+export async function getStaticProps(){
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
+}
+
+export default function Home({ allPostsData }){
   return (
     <Layout home>
       <Head>
@@ -12,6 +22,20 @@ export default function Home(){
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut dolore reprehenderit nemo maxime perferendis vitae voluptatum facilis. Sed, nisi atque hic officiis dolorem optio, quod quos non dignissimos nobis voluptates.</p>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus similique deleniti nulla inventore. Odio natus vero ad ratione repellendus. Ipsum cum totam nihil, sapiente iure doloremque consequatur quisquam vero ex!</p>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident cumque repudiandae fugit aspernatur velit nam, nobis maiores iure sint consectetur fuga, quod illum non impedit est totam, vero consequatur quaerat!</p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   )
