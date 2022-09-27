e0900d04579f0916ef229fe9d09f26eea408c051
Sayed Saddam Hussain <48250591+sayedsaddam@users.noreply.github.com>
2022-09-27 15:49:50 +0500

    updated 7 files including json, js, css and other stuff. deployed the app on vercel.

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
 import Head from "next/head"
+import Link from "next/link"
+import Date from '../components/date'
 import Layout, { siteTitle } from "../components/layout"
 import { getSortedPostsData } from "../lib/posts"
 import utilStyles from '../styles/utils.module.css'
@@ -19,20 +21,24 @@ export default function Home({ allPostsData }){
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut dolore reprehenderit nemo maxime perferendis vitae voluptatum facilis. Sed, nisi atque hic officiis dolorem optio, quod quos non dignissimos nobis voluptates.</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus similique deleniti nulla inventore. Odio natus vero ad ratione repellendus. Ipsum cum totam nihil, sapiente iure doloremque consequatur quisquam vero ex!</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident cumque repudiandae fugit aspernatur velit nam, nobis maiores iure sint consectetur fuga, quod illum non impedit est totam, vero consequatur quaerat!</p>
+        <p>Hi, I am Saddam (Sayed Saddam Hussain), born in Swat in May 1993. Studied Bachelors in Computer Science at the University of Agriculture, Peshawar - Pakistan. Graduated in 2015, started internship at IT Excellence Center, Khyber Pakhtunkhwa in June, 2016 till December 2016.</p>
+        
+        <p>Moved to Innovision Technology (SMC) Pvt. Ltd in January 2017. Worked there till July, 2017 and moved to VIVE Technologies Pvt. Ltd in August 2017.</p>
+
+        <p>Worked at VIVE Technologies for almost a year, then moved to Islamabad to a company called CHIP Training and Consulting Pvt. Ltd as a PHP Developer in March 2019. Worked at CHIP for a year, and moved to AH Group of Companies Pvt. Ltd in Feb 2020. Currently working for AH Group Companies since then.</p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
+              <Link href={`/posts/${id}`} passHref>
+                {title}
+              </Link>
               <br />
-              {id}
-              <br />
-              {date}
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
             </li>
           ))}
         </ul>
